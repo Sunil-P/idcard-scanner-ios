6799ff3b897cab7df19428ea830c73cd9ed233f8
Subhrajyoti Patra <spatra@malwarebytes.com>
2022-10-07 19:04:57 +0300

    Add `FrontendAppKit` and `StorageKit` target.

---
+++ FrontendAppKit/FrontendAppKit.h
@@ -0,0 +1,16 @@
+//
+//  FrontendAppKit.h
+//  FrontendAppKit
+//
+//  Created by Subhrajyoti Patra on 10/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrontendAppKit.
+FOUNDATION_EXPORT double FrontendAppKitVersionNumber;
+
+//! Project version string for FrontendAppKit.
+FOUNDATION_EXPORT const unsigned char FrontendAppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrontendAppKit/PublicHeader.h>
