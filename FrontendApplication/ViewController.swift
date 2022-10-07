04027dcddf7b6c3a72e66a2031ed01edb715fcd2
Subhrajyoti Patra <spatra@malwarebytes.com>
2022-10-07 18:58:16 +0300

    First commit.

---
+++ FrontendApplication/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FrontendApplication
+//
+//  Created by Subhrajyoti Patra on 10/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+} // ViewController
+
